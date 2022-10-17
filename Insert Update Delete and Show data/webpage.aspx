c5e0fb0c3906cdd26f801f2dc54818377a82cf2a
CHANU ABDULLAH <58118089+Hub-of-Abdullah@users.noreply.github.com>
2022-10-17 10:43:27 +0600

    This is Insert Update Delete and show

---
+++ Insert Update Delete and Show data/webpage.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="webpage.aspx.cs" Inherits="Insert_Update_Delete_and_Show_data.webpage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>First Name</td>
+                    <td><asp:TextBox ID="studentFirstName" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Last Name</td>
+                    <td><asp:TextBox ID="studentLastName" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>CLass</td>
+                    <td><asp:TextBox ID="studentClassName" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td> Roll </td>
+                    <td><asp:TextBox ID="studentRollNo" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Result</td>
+                    <td><asp:TextBox ID="studentResult" runat="server"></asp:TextBox></td>
+                </tr>
+                <br />
+
+                <tr>
+                    <td colspan ="100" align="center">
+                        <asp:Button ID="btnStudentInfoSave" runat="server" Text="Save" OnClick="btnStudentInfoSave_Click" />
+                        <asp:Button ID="btnStudentInfoUpdate" runat="server" Text="Update" OnClick="btnStudentInfoUpdate_Click" />
+                        <asp:Button ID="btnStudentInfoDelete" runat="server" Text="Delete" OnClick="btnStudentInfoDelete_Click" />
+                        
+                    </td>
+
+                </tr>
+
+                <tr>
+                    <td>Insert Your Id For Update or Delete</td>
+                    <td><asp:TextBox ID="UpdateDeleteTextBox" runat="server"></asp:TextBox></td>
+                </tr>
+            </table>
+             <br />
+                <asp:GridView ID="StudentInfoDisplayGrid" runat="server"></asp:GridView>
+             <br />
+            
+            
+            <table>
+                 <tr>
+                    <td>Insert Your Class to Show you CLass Information</td>
+                    <td><asp:TextBox ID="classForShow" runat="server"></asp:TextBox></td>
+                </tr>
+              <br />
+               <tr>
+                    <td colspan ="100" align="center">
+                       <asp:Button ID="btnStudentInfoShow" runat="server" Text="Show" OnClick="btnStudentInfoShow_Click"/>
+                    </td>
+                </tr>
+            </table>
+             <br />
+                <asp:GridView ID="EachClassGrid" runat="server"></asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Insert Update Delete and Show data/webpage.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+namespace Insert_Update_Delete_and_Show_data
+{
+    public partial class webpage : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\studentInfomation.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open) { con.Close(); } 
+            con.Open();
+            DisplayData();
+        }
+
+        protected void btnStudentInfoSave_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO studentTable VALUES('"+ studentFirstName.Text+ "','"+ studentLastName.Text+"','"+ studentClassName.Text +"','"+Convert.ToInt32(studentRollNo.Text)+"','"+Convert.ToDouble(studentResult.Text)+"')";
+            cmd.ExecuteNonQuery();
+            studentFirstName.Text = "";
+            studentLastName.Text = "";
+            studentClassName.Text = "";
+            studentRollNo.Text = "";
+            studentResult.Text = "";
+
+            DisplayData();
+        }
+        public void DisplayData()
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT id AS ID, firstName AS FirstName, lastName As LastName, className AS Class, rollNo AS RollNo, Result AS Result  FROM studentTable";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+           
+            StudentInfoDisplayGrid.DataSource = dt;
+            StudentInfoDisplayGrid.DataBind();
+            
+        }
+
+        protected void btnStudentInfoShow_Click(object sender, EventArgs e)
+        {
+
+            DisplayDataEachClassGrid();
+        }
+
+        public void DisplayDataEachClassGrid(int flag )
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM studentTable WHERE className = '" + classForShow.Text + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            EachClassGrid.DataSource = dt;
+            EachClassGrid.DataBind();
+
+            classForShow.Text = "";
+        }
+
+        protected void btnStudentInfoDelete_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "DELETE FROM studentTable WHERE id = '" + Convert.ToInt32(UpdateDeleteTextBox.Text) + "'";
+            cmd.ExecuteNonQuery();
+            classForShow.Text = "";
+            DisplayData();
+            /// DisplayDataEachClassGrid(2);//For update EachClass Grid
+            UpdateDeleteTextBox.Text = "";
+        }
+
+        protected void btnStudentInfoUpdate_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "UPDATE studentTable SET firstName= '"+ studentFirstName.Text + "', lastName = '"+ studentLastName.Text + "', className = '"+ studentClassName.Text +"', rollNo ='"+Convert.ToInt32(studentRollNo.Text) + "', Result = '"+Convert.ToDouble(studentResult.Text)+"'  WHERE id = '" + Convert.ToInt32(UpdateDeleteTextBox.Text) + "'";
+            cmd.ExecuteNonQuery();
+            DisplayData();
+           /// DisplayDataEachClassGrid(2);//For update EachClass Grid
+
+            studentFirstName.Text = "";
+            studentLastName.Text = "";
+            studentClassName.Text = "";
+            studentRollNo.Text = "";
+            studentResult.Text = "";
+            UpdateDeleteTextBox.Text = "";
+        }
+    }
+}
